5a28f00999c2e504025178b975440188c5e8744f
neelabhsinha <neelabhsinha97@gmail.com>
2024-07-30 00:50:59 +0530

    Task Instance Classification Generation - Validating from actual set

--- src/utils/task_instance_classifications_generator.py
+++ src/utils/task_instance_classifications_generator.py
@@ -1,6 +1,6 @@
 from src.data.experimental_dataset import ExperimentalDataset
 from src.model.gpt_3_5_turbo import GPT35Turbo
-from const import dataset_to_text_type_mapping, experimental_set
+from const import dataset_to_text_type_mapping, experimental_set, valid_knowledge_types, valid_application_domains
 from src.utils.json_utils import save_dict_to_json
 
 import os
@@ -23,16 +23,27 @@ def generate_task_instance_classifications(split='test'):
             classification_type = dataset_to_text_type_mapping[data['dataset']]
         domains = classification_generator.get_image_classification(question, caption, object_tags,
                                                                     'application_domain')
+        domains = domains['application_domain'] if 'application_domain' in domains else ['Other']
+        domains = filter_elements(domains, valid_application_domains)
         knowledge_types = classification_generator.get_image_classification(question, caption, object_tags,
                                                                             'knowledge_type')
+        knowledge_types = knowledge_types['knowledge_type'] if 'knowledge_type' in knowledge_types else ['Other']
+        knowledge_types = filter_elements(knowledge_types, valid_knowledge_types)
         classifications = {
             'dataset': data['dataset'],
             'key': data['key'],
             'task_type': classification_type,
-            'application_domain': domains['application_domain'] if 'application_domain' in domains else 'Other' ,
-            'knowledge_type': knowledge_types['knowledge_type'] if 'knowledge_type' in knowledge_types else 'Other'
+            'application_domain': domains,
+            'knowledge_type': knowledge_types
         }
         file_name = dataset[idx]['data']['dataset'] + '_' + str(dataset[idx]['data']['key']) + '.json'
         dir_name = f'{experimental_set}/{split}/task_instance_classifications'
         os.makedirs(dir_name, exist_ok=True)
         save_dict_to_json(os.path.join(dir_name, file_name), classifications)
+
+
+def filter_elements(input_list, categories):
+    result = [element for element in input_list if element in categories]
+    if len(result) == 0:
+        result.append('Other')
+    return result
